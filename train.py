b8b27d8fad1ab64a487ae682cafffb8d3789d63f
Txus <me@txus.io>
2019-09-06 14:56:30 +0200

    Add metrics

--- train.py
+++ train.py
@@ -2,10 +2,34 @@ from pathlib import Path
 import click
 from fastai.text import language_model_learner, TransformerXL
 from fastai.basics import load_data
-from fastai.callbacks import SaveModelCallback
+from fastai.callbacks import SaveModelCallback, LearnerCallback
+from fastai.callback import MetricsList
 from fastai.distributed import *
+from fastai.distributed import ifnone
 import torch
 import warnings
+from typing import Any
+import json
+from time import time
+
+
+class PaperspaceLoggingCallback(LearnerCallback):
+    def on_train_begin(self, **kwargs: Any) -> None:
+        for name in self.learn.recorder.names:
+            print(json.dumps(dict(chart=name, axis=name)))
+
+    def on_epoch_begin(self, **kwargs: Any) -> None:
+        if self.add_time:
+            self.start_epoch = time()
+
+    def on_epoch_end(self, epoch: int, smooth_loss: torch.Tensor,
+                     last_metrics: MetricsList, **kwargs: Any) -> bool:
+        "Add a line with `epoch` number, `smooth_loss` and `last_metrics`."
+        last_metrics = ifnone(last_metrics, [])
+        for name, stat in zip(self.learn.recorder.names,
+                              [epoch, smooth_loss] + last_metrics):
+            when = time() - self.start_epoch
+            print(json.dumps(dict(chart=name, x=when, y=stat)))
 
 
 @click.command()
@@ -36,11 +60,15 @@ def main(lm_databunch: str, model_path: str, encoder: str, itos: str,
     learn = learn.to_distributed(local_rank)
     learn.freeze()
     click.echo("Training model head...")
-    learn.fit_one_cycle(4, slice(1e-3), callbacks=[SaveModelCallback(learn)])
+    learn.fit_one_cycle(4, slice(
+        1e-3), callbacks=[PaperspaceLoggingCallback(learn),
+                          SaveModelCallback(learn)])
     learn.load("bestmodel")
     learn.unfreeze()
     click.echo("Unfreezing and fine-tuning earlier layers...")
-    learn.fit_one_cycle(4, slice(1e-3), callbacks=[SaveModelCallback(learn)])
+    learn.fit_one_cycle(4, slice(
+        1e-3), callbacks=[PaperspaceLoggingCallback(learn),
+                          SaveModelCallback(learn)])
     learn.load("bestmodel")
     click.echo("Validating...")
     click.echo(learn.validate())
